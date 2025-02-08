c286095e1afd88bd053b605a5878c5de00a072eb
EduSocrates <edu5390@gmail.com>
2025-02-08 16:39:13 -0300

    aplicando alteracoes para rodar no docker corretamente

--- src/main/java/com/fiap/RM358568/edusocrates/controle_restaurante/dominio/DTO/AvaliacaoDTO.java
+++ src/main/java/com/fiap/RM358568/edusocrates/controle_restaurante/dominio/DTO/AvaliacaoDTO.java
@@ -1,13 +1,13 @@
 package com.fiap.RM358568.edusocrates.controle_restaurante.dominio.DTO;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Getter
+@Setter
 public class AvaliacaoDTO {
     private Long id;
     private int nota;
